b0949c0092dce47357d548a988e35d5ea6e3e8be
Dostmurod <dostmurodzoirov777@gmail.com>
2022-08-20 00:50:53 +0500

    Function practik

---
+++ app.js
@@ -0,0 +1,83 @@
+// 1-misol
+
+// let x = 2;
+
+// function incrument() {
+// //     return Math.pow(x, 2);
+// }
+// console.log(incrument(x));
+
+
+// 2-misol
+
+// function sonlarYigindisi (a,b){
+//     console.log(a+b);
+// }
+
+// sonlarYigindisi(55 , 55);
+
+// 3-misol
+
+// function sonlarYigindisi (a,b){
+//     console.log(a-b);
+// }
+
+// sonlarYigindisi(55 , 5);
+
+// 4-misol
+
+// let x = 2;
+
+// function incrument() {
+//     let one = Math.pow(x, 2);
+//     let one2 = Math.pow(x, 3);
+//     let one3 = Math.pow(x, 4);
+//     let sum = `${one} , ${one2} , ${one3}`
+//     return sum
+// }
+// console.log(incrument(x));
+
+// 5 - 6 misollar
+
+// function son(number){
+//     if(number% 2===0){
+//         console.log(`${number} juft `);
+//     }else{
+//         console.log(`${number} toq`);
+//     }
+// }
+
+// son(2001)
+
+// 7-misol
+// let n = 3;
+// function incrument(incrumentNumber , decrumentNumber){
+//     let inc = ++incrumentNumber;
+//     let dec = ++decrumentNumber;
+
+//     return `${inc} , ${dec}`
+// }
+// console.log(incrument(n , n));
+
+// 8-misol
+
+// let n = +prompt('sonni kiriting');
+ 
+// for (let i = 2; i * 2 <= n; i++) {
+//     if (n % i == 0) {
+//         alert(`${n} boluvchilari: ${i}`);
+//     }
+// }
+
+
+// 10-misol
+
+// const number = parseInt(prompt('Sonni kiriting '));
+
+// for(let i = 1; i <= 10; i++) {
+
+//     const result = i * number;
+
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
